318971a5d761fa4fff98d7006347c6c355becabc
tamim <tamimhossain6421@gmail.com>
2024-02-11 23:11:17 +0600

    added navbar and banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
+import Banner from '@/components/Banner/Banner';
+import Navbar from '@/components/Navbar/Navbar';
 import React from 'react';
 
 const HomeLayout = () => {
   return (
     <div>
+      <Navbar></Navbar>
+      <div className="">
+        <Banner></Banner>
       <p>this is home </p>
+      </div>
     </div>
   );
 };
